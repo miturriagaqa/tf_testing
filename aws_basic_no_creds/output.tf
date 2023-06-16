3926e45c747a3681b27e3142d537c539e89cc716
miturriagaqa <109548388+miturriagaqa@users.noreply.github.com>
2023-06-16 09:25:40 -0400

    Update output.tf

--- aws_basic_no_creds/output.tf
+++ aws_basic_no_creds/output.tf
@@ -12,3 +12,16 @@ output "instance_name" {
   description = "name of the EC2 instance"
   value       = var.instance_name
 }
+
+output "private_ip" {
+  value = aws_instance.app_server.private_ip
+}
+
+output "vpc_security_group_ids" {
+  value = aws_instance.app_server.vpc_security_group_ids
+}
+
+output "availability_zone" {
+  value = aws_instance.app_server.availability_zone
+}
+
