5d7e12c55cb8d2b442de77da1a37066a083c9bc3
miturriagaqa <109548388+miturriagaqa@users.noreply.github.com>
2023-06-28 10:01:50 -0400

    Update main.tf

---
+++ aws_mish_key_pair/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 0.13"
+}
+
+data "aws_key_pair" "existing_keypair" {
+  key_name = "mish_key_pair"
+}
+
+provider "aws" {
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "app_server"{
+  ami           = "ami-0b301ce3ce347599c"
+  instance_type = "t2.micro"
+  key_name = data.aws_key_pair.existing_keypair.key_name
+  
+  tags = {
+    Name = var.instance_name
+  }
+}
