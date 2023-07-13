805766b78a116b9e5b5d24c80fb52b425ee6c39c
miturriagaqa <109548388+miturriagaqa@users.noreply.github.com>
2023-07-13 16:03:19 -0400

    Update main.tf

---
+++ var_map_test/main.tf
@@ -0,0 +1,26 @@
+variable "map_type_test" {
+  type = object({ name : string, content : number })
+  default = {
+    name    = "Mabel",
+    content = 52
+  }
+}
+
+
+
+resource "local_file" "foo" {
+  filename = "/tmp/${var.map_type_test["name"]}"
+  content  = <<-EOT
+    resource_handler = ${var.resource_handler}
+    something static = ${var.static}
+    ${var.map_type_test["content"]}
+    %{for color in var.list_type_test~}
+list item = ${color}
+    %{endfor~}
+Bool vaule = ${var.bool_type_test}
+  EOT
+}
+
+output "my_map" {
+  value = var.map_type_test
+}
