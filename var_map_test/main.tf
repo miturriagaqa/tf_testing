a87f83404e4f2cea3ea053bfffcdf6ba7229e8fc
miturriagaqa <109548388+miturriagaqa@users.noreply.github.com>
2023-07-13 16:11:42 -0400

    Update main.tf

--- var_map_test/main.tf
+++ var_map_test/main.tf
@@ -1,12 +1,34 @@
 variable "map_type_test" {
-  type = object({ name : string, content : string })
+  type = object({ name : string, content : number })
   default = {
     name    = "Mabel",
-    content = "hello
+    content = 52
   }
 }
 
+variable "list_type_test" {
+  type    = list(any)
+  default = ["red", "yellow", "green"]
+}
 
+variable "bool_type_test" {
+  type    = bool
+  default = false
+}
+
+variable "null_type_test" {
+  default = "default if not supplied or null"
+}
+
+variable "resource_handler" {
+  type    = string
+  default = ""
+}
+
+variable "static" {
+  type    = number
+  default = 0
+}
 
 resource "local_file" "foo" {
   filename = "/tmp/${var.map_type_test["name"]}"
@@ -23,4 +45,24 @@ Bool vaule = ${var.bool_type_test}
 
 output "my_map" {
   value = var.map_type_test
+}
+
+output "my_list" {
+  value = var.list_type_test
+}
+
+output "my_bool" {
+  value = var.bool_type_test
+}
+
+output "my_rh" {
+  value = var.resource_handler
+}
+
+output "my_static_number" {
+  value = var.static
+}
+
+output "my_nullable" {
+  value = var.null_type_test != "null" ? var.null_type_test : "default because null"
 }
